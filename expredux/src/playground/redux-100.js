7678d2da6a5ac464b82cc396220498f181ebe539
fleria kate <fleriakate@gmail.com>
2020-03-12 17:13:35 +0300

    redux

--- expredux/src/playground/redux-100.js
+++ expredux/src/playground/redux-100.js
@@ -2,14 +2,24 @@ import {  createStore } from 'redux';
 
 const store = createStore((state = { count: 0},action) => {
    switch (action.type) {
-       case 'INCREMENT':
+       case 'INCREMENT':1
            return {
-               count: state.count + 1
+               count: state.count + action.incrementBy
            }
-         case 'DECREMENT':
+         case 'DECREMENT':1
                 return {
-                    count :state.count -1
+                    count :state.count - action.decrementBy
                }
+             
+            
+          case 'RESET':
+              return {
+                  count:0
+              }
+              case 'SET':
+                  return {
+                  const :action.count
+              }    
          default:
              return state;  
    }
@@ -20,18 +30,52 @@ store.subscribe ((() => {
 }))
 
 //Actions  -> an object  that gets sent to the store
+
+//Action-creator - functions that return action objects
+const incrementCount = (payload = {}) => {
+    return {
+        type: 'INCREMENT',
+        incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy
+        : 1
+    }
+}
+
+const decrementCount = (payload = {}) => {
+    return {
+        type: 'DECREMENT',
+        decrementBy:typeof payload.decrementBy === 'number'? payload.decrementBy
+        : 1    
+    }
+}
+     
+store.dispatch(incrementCount({incrementBy:5}));
+store.dispatch(incrementCount());
+
+store.dispatch(decrementCount({decrementBy:10}));
+store.dispatch(decrementCount());
+
+
 store.dispatch({
     type: 'INCREMENT'
 });
-        
+
 store.dispatch({
-    type: ' INCREMENT'
-});
+    type: 'RESET'
+})
+
 
 store.dispatch({
     type: 'DECREMENT'
 });
 
 
+store.dispatch({
+    type: 'DECREMENT'
+});
 
+
+store.dispatch({
+    type: 'SET',
+    count:150
+})
 
