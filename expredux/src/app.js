d76df478aa5593b403be39c7ab4554a726d4a261
fleria kate <fleriakate@gmail.com>
2020-03-12 10:32:11 +0300

    react - router

--- expredux/src/app.js
+++ expredux/src/app.js
@@ -1,40 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter , Route } from "react-router-dom";
+import AppRouter from './routers/AppRouter';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-        <div>
-            This is from my dashboard page
-        </div>
-    )
 
-    const AddExpensePage = () => (
-        <div>
-            This is from my add expense component
-        </div>
-    )
-
-    const EditExpensePage = () => (
-        <div>
-            This is from my edit expense component
-        </div>
-    )
-
-    const HelpPage = () => (
-        <div>
-            This is from my help component
-        </div>
-    )
-
-    const routes = (
-        <BrowserRouter>
-        <Route path="/" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        </BrowserRouter>
-    )
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />,document.getElementById('app'));
---
+++ expredux/src/components/AddExpensePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+)
+
+export default AddExpensePage;
+
+//EditExpensePage.js
+//ExpenseDashboardPage.js
+//Header.js
+//HelPage.js
+//NotFoundPage.js
