d8431b0e9f8ecdf0279a6c8c16c15c02257c8ce3
fleria kate <fleriakate@gmail.com>
2020-03-12 11:50:05 +0300

    redux

--- expredux/src/playground/redux-100.js
+++ expredux/src/playground/redux-100.js
@@ -1,1 +1,37 @@
-console.log('javascript is awesome');
+import {  createStore } from 'redux';
+
+const store = createStore((state = { count: 0},action) => {
+   switch (action.type) {
+       case 'INCREMENT':
+           return {
+               count: state.count + 1
+           }
+         case 'DECREMENT':
+                return {
+                    count :state.count -1
+               }
+         default:
+             return state;  
+   }
+});
+
+store.subscribe ((() => {
+    console.log(store.getState());
+}))
+
+//Actions  -> an object  that gets sent to the store
+store.dispatch({
+    type: 'INCREMENT'
+});
+        
+store.dispatch({
+    type: ' INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+
+
+
