b78b3c1e51752e095770c309e1ac669cf2cc4711
fleria kate <fleriakate@gmail.com>
2020-03-13 16:53:09 +0300

    filtersReducer

--- expredux/src/playground/exp-manage.js
+++ expredux/src/playground/exp-manage.js
@@ -21,39 +21,41 @@ import { createStore,combineReducers }  from 'redux';
 import uuid from 'uuid';
 
 //ADD EXPENSE
-// const addExpense = (expense = {}) => {
+const addExpense = (expense = {}) => {
+    return {
+
+        type:'ADD_EXPENSE',
+        expense:{
+            id: uuid(),
+            description:expense.description ? expense.description : '',
+            note: expense.note ? expense.note:'',
+            amount: expense.amount ? expense.amount : 0,
+            createdAt: expense.createdAt ? expense.createdAt : 0
+
+        }
+    }
+}
+
+
+// const addExpense = (
+//     {
+//         description = '',
+//         note = '',
+//         amount = 0,
+//         createdAt = 0
+//       } = {}
+// ) => {
 //     return {
 //         type:'ADD_EXPENSE',
 //         expense:{
-//             id: uuid(),
-//             description:expense.description ? expense.description : '',
-//             note: expense.note ? expense.note:'',
-//             amount: expense.amount ? expense.amount : 0,
-//             createdAt: expense.createdAt ? expense.createdAt : 0
-
-//         }
+//             id:uuid(),
+//             description,
+//             note,
+//             amount,
+//             createdAt
 //     }
 // }
-
-const addExpense = (
-    {
-        description = '',
-        note = '',
-        amount = 0,
-        createdAt = 0
-      } = {}
-) => {
-    return {
-        type:'ADD_EXPENSE',
-        expense:{
-            id:uuid(),
-            description,
-            note,
-            amount,
-            createdAt
-    }
-}
-}
+// }
 
 //REMOVE_EXPENSE
 const removeExpense = (expense = {}) => {
@@ -63,19 +65,46 @@ const removeExpense = (expense = {}) => {
     }
 }
 
+//EDIT_EXPENSE
+const editExpense = (id,updates) => {
+    return {
+        type: 'EDIT_EXPENSE',
+        id,
+        updates
+    }
+}
+
+//SET_TEXT_FILTER
+const setTextFilter = (text = '') => {
+    return {
+        type: 'SET_TEXT_FILTER',
+        text
+    }
+}
+
 //Expenses Reducer
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState,action) => {
     switch (action.type) {
-        // case 'ADD_EXPENSE':
-        //     return [
-        //         ...state,THIS IS FOR ADDING EXPENSE
-        //         action.expense
-            //] //Object.assign({},state,action.expense)
+        case 'ADD_EXPENSE':
+            return [
+                ...state,//THIS IS FOR ADDING EXPENSE,can use push ,concat and spreadOperator
+                action.expense
+            ] //Object.assign({},state,action.expense)
             case 'REMOVE_EXPENSE':
                 return state.filter((expense) => expense.id !== action.id);
+            case 'EDIT_EXPENSE':
+                    return state.map((expense) => {
+                        if (expense.id === action.id) {
+                            return Object.assign({},expense, action.updates)
+                        } else {
+                            return expense;
+                        }
+                    })
+                    
+
         default:
-            return state;
+            return state; 
     }
 }
 
@@ -89,14 +118,17 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState,action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return Object.assign({},state,{
+                text:action.text
+            });
         default:
             return state;
     }
 }
 
 //Store creation
-const store = createStore(
-    combineReducers({
+const store = createStore(combineReducers({
         expenses: expensesReducer,
         filters: filtersReducer
     })
@@ -108,7 +140,6 @@ store.subscribe(() => {
 })
 const expenseOne = store.dispatch(addExpense({description:'Vacation',amount:500}
 ));
-
 const expenseTwo = store.dispatch(addExpense({description:'Coffee',amount:600 })
 );
 
@@ -117,4 +148,5 @@ const expenseFour = store.dispatch(addExpense());
 
 store.dispatch(removeExpense({id:expenseThree.expense.id}));
 
-
+store.dispatch(editExpense(expenseTwo.expense.id,{ description:'Tea'}));
+store.dispatch(setTextFilter('sport'));
