65254cb56a262c4488114d8ec047e1e9f85eae8e
fleria kate <fleriakate@gmail.com>
2020-03-18 22:44:54 +0300

    code wars

---
+++ code wars/slice&substr.js
@@ -0,0 +1,53 @@
+var arr ="cde";
+console.log(arr.slice(0,2));
+
+function cutIt (arr){var arr = "fgh";
+console.log(arr.slice(0,2));}
+var arr = "cde";
+function cutIt (arr){
+    console.log(arr.slice(0,2));
+};
+var arr = "cde";
+function cutIt (arr){console.log(arr.slice(0,2))};
+
+function cutIt (arr){
+   var arr = "cde"
+   console.log(arr.slice(0,2));
+}
+var arr = ("cde")
+function cutIt (arr){console.log(arr.slice(0,2))};
+
+function cutIt (arr){
+    console.log(arr.slice(0,2));
+}
+cutIt(arr);
+
+var arr = 'cde';
+console.log(arr.slice(0,2));
+
+var arr = 'cde';
+function cutIt (arr){console.log(arr.slice(0,2))};
+cutIt(arr);
+
+function cutIt (arr) {console.log(arr.slice)(0,2)};
+var arr = 'cde';
+cutIt(arr);
+
+function cutIt(arr){ var arr = 'cde'
+console.log(arr.slice)(0,2)};
+cutIt(arr);
+
+function cutIt (arr){
+    var arr = 'cde'
+    console.log(arr.slice)(0,2)
+    return arr ;
+}
+cutIt(arr);
+
+function cutIt (arr){
+    var arr = 'cde'
+    console.log(arr.slice)(0,2)
+};
+cutIt(arr);
+
+f
