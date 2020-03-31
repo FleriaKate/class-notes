3160076dd7fb79e9e8f9b5d4ea837563a325e81e
fleria kate <fleriakate@gmail.com>
2020-03-31 12:36:47 +0300

    expredux

--- expredux/src/components/ExpenseListFilters.js
+++ expredux/src/components/ExpenseListFilters.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {} from '../actions'
-
+6
 const ExpenseListFilters = props => {
     return (
         <div>
-        <input  type ="text" value={mapStateToPropst.filters.text} />
+        <input  type ="text"
+         value={mapStateToPropst.filters.text}
+         onChange={e => {
+             props.dispatch(setTextFilter(e.target.value));
+         }} 
+         />
+         <select 
+             value={props.filters.sortBy}
+             onChange={e => {
+                     if (e.target.value === "amount"){
+                         props.dispatch(sortByAmount());
+                     }else if (e.target.value === "amount") {
+                         props.dispatch(sortByAmount());
+                     }
+                 }}
+                 >
+                <option value="date">Date</option>
+                 <option value="amount">Amount</option>
+         </select>
         </div>
     );
 };
